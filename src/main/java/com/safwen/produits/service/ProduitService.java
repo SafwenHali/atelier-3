d796b0e4d8f44801f1c732d8d8b5467fc14c706d
SafwenHali <93746781+SafwenHali@users.noreply.github.com>
2022-04-15 01:53:26 +0200

    upload

---
+++ src/main/java/com/safwen/produits/service/ProduitService.java
@@ -0,0 +1,18 @@
+package com.safwen.produits.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.safwen.produits.entities.Produit;
+
+public interface ProduitService {
+	Produit saveProduit(Produit p);
+	Produit updateProduit(Produit p);
+	void deleteProduit(Produit p);
+	 void deleteProduitById(Long id);
+	Produit getProduit(Long id);
+	List<Produit> getAllProduits();
+	
+	Page<Produit> getAllProduitsParPage(int page, int size);
+}
